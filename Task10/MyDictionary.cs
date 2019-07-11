943575c280483c8958566bc725144c5728fc7b99
Slavik <slavik.kuz.mail@gmail.com>
2019-07-11 15:26:15 +0200

    Task 10

---
+++ Task10/MyDictionary.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class MyDictionary<Tkey, Tvalue>:IEnumerable<object>, IEnumerator<object>
+    {
+        readonly Tkey[] tKey;
+        readonly Tvalue[] tValue;
+        readonly int length;
+
+        int position = -1;
+
+        public MyDictionary(int n)
+        {
+            tKey = new Tkey[n];
+            tValue = new Tvalue[n];
+            length = n;
+        }
+
+        public void Add(int i, Tkey a, Tvalue b)
+        {
+            tKey[i] = a;
+            tValue[i] = b;
+        }
+
+        public int Length
+        {
+            get { return length; }
+        }
+
+        public object Current
+        {
+            get { return tKey[position] + " " + tValue[position]; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return tKey[position] + " " + tValue[position]; }
+        }
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= tKey.Length)
+                    return "Key: " + tKey[index].ToString() + ". Value: " + tValue[index].ToString() + ".";
+                return "Request is outside of boundaries!";
+            }
+        }
+
+        IEnumerator<object> IEnumerable<object>.GetEnumerator()
+        {
+            return this;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public void Dispose()
+        {
+            Reset();
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        public bool MoveNext()
+        {
+            if (position < tKey.Length-1)
+            {
+                position++;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Task10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dictionary = new MyDictionary<string, string>(3);
+
+            dictionary.Add(0, "bord", "table");
+            dictionary.Add(1, "eple", "aplle");
+            dictionary.Add(2, "sol", "sun");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("Second line in the dictionary:");
+            Console.WriteLine(dictionary[1]);
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("Size:");
+            Console.WriteLine(dictionary.Length + " words");
+            Console.WriteLine(new string('-', 50));
+
+            Console.ReadKey();
+
+        }
+    }
+}
