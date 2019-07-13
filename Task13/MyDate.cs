11543e3c4365fb9d3c4e4644cf62b5d622a8c085
Slavik <slavik.kuz.mail@gmail.com>
2019-07-13 14:21:28 +0200

    Task 13

---
+++ Task13/MyDate.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13
+{
+    class MyDate
+    {
+        readonly DateTime date;
+
+        public MyDate(DateTime date)
+        {
+            this.date = date;
+        }
+
+        public static MyDate operator - (MyDate d1, MyDate d2)
+        {
+            TimeSpan timeSpan = d1.date - d2.date;
+            DateTime newDate = d1.date.AddDays(timeSpan.Days);
+            return new MyDate(newDate);
+        }
+
+        public static MyDate operator +(MyDate d1, MyDate d2)
+        {
+            TimeSpan timeSpan = d1.date - d2.date;
+            DateTime newDate = d1.date.AddDays(-timeSpan.Days);
+            return new MyDate(newDate);
+        }
+
+        public override string ToString()
+        {
+            return "Date: " + date;
+        }
+
+        public static MyDate Sub(MyDate d1, MyDate d2)
+        {
+            return d1 - d2;
+        }
+
+        public static MyDate Add(MyDate d1, MyDate d2) 
+        {
+            return d1 + d2;
+        }
+
+    }
+}
---
+++ Task13/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MyDate date1 = new MyDate(DateTime.Now);
+            Console.WriteLine(date1.ToString()); 
+
+            MyDate date2 = new MyDate(new DateTime(2017, 12, 4));
+            Console.WriteLine(date2.ToString());
+
+            Console.WriteLine(MyDate.Sub(date1, date2).ToString());
+            Console.WriteLine(MyDate.Add(date1, date2).ToString());
+
+            // Delay.
+            Console.ReadKey();
+
+        }
+    }
+}
