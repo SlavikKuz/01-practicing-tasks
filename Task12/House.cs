e57e9c42c0f2e698fe9258eb275186e7110ccd57
Slavik <slavik.kuz.mail@gmail.com>
2019-07-13 12:30:59 +0200

    task 12

---
+++ Task12/House.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class House
+    {
+        string city;
+        Street street;
+
+        public string City
+        {
+            set { city = value; }
+            get { return city; }
+        }
+
+        public Street Street
+        {
+            set { street = value; }
+            get { return street; }
+        }
+
+        public House(Street street, string city)
+        {
+            this.city = city;
+            this.street = street;
+        }
+
+        public House Clone()
+        {
+            return this;
+        }
+
+        public House DeepClone()
+        {
+            return new House(this.Street.Clone() as Street, this.City);
+        }
+    }
+
+    class Street:ICloneable
+    {
+        string str;
+
+        public string Str { get { return str; } }
+
+        public Street(string street)
+        {
+            str = street;
+        }
+
+        public object Clone()
+        {
+            return new Street(this.str);
+        }
+    }
+}
---
+++ Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            House hou1 = new House(new Street("Ramberveien"), "Toensberg");
+
+            House clone = hou1.DeepClone();
+
+            Console.WriteLine(clone.City + " " + clone.Street.Str);
+
+            Console.ReadKey();
+        }
+    }
+}
