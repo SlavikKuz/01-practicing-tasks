bca7821639a937ab3b8e2f520d56670a75cf60cc
Slavik <slavik.kuz.mail@gmail.com>
2019-07-20 18:07:48 +0200

    Tasks 01-09

---
+++ Task16/Citizen.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task16
+{
+    abstract class Citizen
+    {
+        public string Pass { protected set; get; }
+        public string FirstName { protected set;  get;  }
+        public string LastName { protected set; get; }
+        public string FullName
+        {
+            get { return string.Format("{0} {1}", FirstName, LastName); }
+        }
+
+
+    }
+}
